5750d534469926dae59f6e13c238cc777788b50c
Abdulatif Jalolov <abdulatifjalolov6004273@gmail.com>
2022-10-24 13:00:17 +0500

    cleared all

--- facebook/src/main/java/org/example/Main.java
+++ facebook/src/main/java/org/example/Main.java
@@ -2,13 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println( " Hello");
-        System.out.println("iroda");
-        System.out.println("Abdulatif");
-        System.out.println("Bekzod");
-        System.out.println("IRODA");
-        System.out.println("edited by abdulatif");
-        System.out.println("jgfjhclihkvkjv");
+
     }
 }
