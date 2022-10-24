e5ec9369abec902699f227b168461ff3613f1e68
Abdulatif Jalolov <abdulatifjalolov6004273@gmail.com>
2022-10-24 13:24:42 +0500

    jasur

--- facebook/src/main/java/org/example/Main.java
+++ facebook/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("something edited");
+        System.out.println("edited by jasur");
     }
 }
