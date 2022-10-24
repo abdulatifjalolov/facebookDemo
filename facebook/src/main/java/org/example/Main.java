fb15ae50b9f842d1f3a2b611a51cb1625957ae53
Abdulatif Jalolov <abdulatifjalolov6004273@gmail.com>
2022-10-24 16:56:48 +0500

    abdulatif

--- facebook/src/main/java/org/example/Main.java
+++ facebook/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("edited by jasur");
+        System.out.println("1.LOG IN 2.REGISTRATION 0.EXIT");
+
     }
 }
